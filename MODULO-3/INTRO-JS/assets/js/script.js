311de6b94f704438dba40705fac42a78cc8240f8
Ivan Iraldi, SykoTech <iraldiban@gmail.com>
2026-06-23 22:22:39 -0300

    Intro a js

---
+++ MODULO-3/INTRO-JS/assets/js/script.js
@@ -0,0 +1,12 @@
+// para definir una variable SIEMPRE tenemos que usar una palabra clave (var, let o const)
+// var tiene un alcance GLOBAL
+// let tiene alcance de bloque  LET puede cambiar su valor
+// const tiene alcance de bloque  CONST NO puede cambiar su valor
+
+var nombre = "Ivan" // STRING (cadena de texto)
+var edad = 27 // NUMBER
+var decimal = 27.55
+
+var programador = true // TIPO DE DATO BOOLEAN (true/false)
+
+alert(nombre)
